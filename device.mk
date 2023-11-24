162adb11f1d4cbbfd6d65317ffdef5ae55c50f26
Asriadi Rahim <asriadirahim03@gmail.com>
2023-11-25 02:25:06 +0800

    wahoo: Pin SystemUIGoogle instead SystemUI * Evox use vendor_pixel_framework
    Signed-off-by: Asriadi Rahim <asriadirahim03@gmail.com>

--- device.mk
+++ device.mk
@@ -748,7 +748,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
+    SystemUIGoogle
 
 # audio effects config
 PRODUCT_PROPERTY_OVERRIDES += \
