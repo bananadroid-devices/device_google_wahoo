36095f9e665b79b722371266be2aa400749b4639
Asriadi Rahim <asriadirahim03@gmail.com>
2024-01-21 15:41:19 +0000

    wahoo: Ship ViMusic prebuilt * Credite: https://github.com/vfsfitvnm/ViMusic/releases/tag/v0.5.4
    Signed-off-by: Asriadi Rahim <asriadirahim03@gmail.com>

--- device.mk
+++ device.mk
@@ -417,6 +417,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ElmyraService
 
+# ViMusic
+PRODUCT_PACKAGES += \
+    ViMusic
+
 # NowPlayingOverlay
 PRODUCT_PACKAGES += \
     NowPlayingOverlay
